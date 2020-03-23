77f113d99d6493888f0dc34c7b1ef269279a8104
Mack Maier <hmaiermack@gmail.com>
2020-03-23 14:24:14 -0700

    added dummy data, set up api token and env

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('ballin!')
-})
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+
+function handleGetTypes(req, res){
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes)
 
 const PORT = 8000
 
